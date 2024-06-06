311fffd79e3be79da8b18dc79ee83e324f57a747
kuriakm <kuriakose_ma@mailfence.com>
2024-06-05 22:18:05 -0400

    Modified review layout

--- src/components/review.jsx
+++ src/components/review.jsx
@@ -89,20 +89,3 @@ const Review = (props) => {
 };
 
 export default Review;
-
-/*  <div className="reviews">
-        <div className="review-head">
-          <h4>{review.reviewer}</h4>
-        </div>
-        <p>{review.content}</p>
-        <div id="review-info">
-          <a href="#" id="edit" onClick={openEditDialog}>
-            &#9998;
-          </a>
-
-          <a href="#" id="remove">
-            &#x2715;
-          </a>
-          <p className="rating">{review.rating}/5</p>
-        </div>
-      </div> */
